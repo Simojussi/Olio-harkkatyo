84d44fb1c5e6249f5c19569d6fdee390585a02e2
Simojussi <sjsuonpaa@gmail.com>
2023-04-09 16:12:24 +0300

    Harkkatyo_v1

---
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.lutemon_fighter_ultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/lutemon_fighter_ultimate/MainActivity.java
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lutemon_fighter_ultimate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void toHome(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void toLutemonList(View view){
+        Intent intent = new Intent(this, LutemonListActivity.class);
+        startActivity(intent);
+    }
+
+    public void toTraining(View view){
+        Intent intent = new Intent(this, TrainingActivity.class);
+        startActivity(intent);
+    }
+
+    public void toBattle(View view){
+        Intent intent = new Intent(this, BattleActivity.class);
+        startActivity(intent);
+    }
+
 }
