afa549cbb5cf1d56fff3a3bccf9e2b6c506b72ff
Simojussi <sjsuonpaa@gmail.com>
2023-04-13 17:41:57 +0300

    Harkkatyo_v1

---
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/Lutemon.java
@@ -0,0 +1,20 @@
+package com.example.lutemon_fighter_ultimate;
+
+public class Lutemon {
+    protected String name;
+    protected String color;
+    protected int attack;
+    protected int defense;
+    protected int experience;
+    protected int health;
+    protected int maxHealth;
+    protected int id;
+    private int idCounter;
+    protected int image;
+
+    public Lutemon(String name, String color, int attack, )
+}
+
+
+
+
