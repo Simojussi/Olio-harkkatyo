b3be8e9f73de7c15a5753accb46b1bb8d302d1f0
henrisaksman <123003493+henrisaksman@users.noreply.github.com>
2023-04-20 16:40:35 +0300

    Tässä muutokset

--- app/src/main/java/com/example/lutemon_fighter_ultimate/Lutemon.java
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/Lutemon.java
@@ -1,6 +1,6 @@
 package com.example.lutemon_fighter_ultimate;
 
-public class Lutemon {
+public class Lutemon extends Storage {
     protected String name;
     protected String color;
     protected int attack;
@@ -12,7 +12,69 @@ public class Lutemon {
     private int idCounter;
     protected int image;
 
-    public Lutemon(String name, String color, int attack, )
+    public Lutemon(String name, String color, int attack, int defense, int experience, int health, int maxHealth, int id, int idCounter, int image) {
+        this.name = name;
+        this.color = color;
+        this.attack = attack;
+        this.defense = defense;
+        this.experience = experience;
+        this.health = health;
+        this.maxHealth = maxHealth;
+        this.id = id;
+        this.idCounter = idCounter;
+        this.image = image;
+    }
+
+    public int Attack() {
+        attack = lutemons.;
+        return attack;
+    }
+
+    public void Defense() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getIdCounter() {
+        return idCounter;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+
 }
 
 
---
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/Storage.java
@@ -0,0 +1,11 @@
+package com.example.lutemon_fighter_ultimate;
+
+import java.util.ArrayList;
+
+public class Storage {
+    protected String name;
+    protected ArrayList<Storage> lutemons = new ArrayList<>();
+
+
+
+}
