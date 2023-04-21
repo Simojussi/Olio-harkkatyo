d8d9fb450d41609499585b784f8ec4709c6ee2b2
henrisaksman <123003493+henrisaksman@users.noreply.github.com>
2023-04-21 13:33:33 +0300

    Tässä muutokset

--- app/src/main/java/com/example/lutemon_fighter_ultimate/LutemonListAdapter.java
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/LutemonListAdapter.java
@@ -28,11 +28,11 @@ public class LutemonListAdapter extends RecyclerView.Adapter<LutemonViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull LutemonViewHolder holder, int position) {
 
-        holder.txtName.setText(String.valueOf(lutemons.get(position).getName()));
-        holder.txtAttack.setText(String.valueOf(lutemons.get(position).getAttack()));
-        holder.txtDefence.setText(String.valueOf(lutemons.get(position).getDefense()));
-        holder.txtHealth.setText(String.valueOf(lutemons.get(position).getHealth()) + "/" + String.valueOf(lutemons.get(position).getMaxHealth()));
-        holder.txtExperience.setText(String.valueOf(lutemons.get(position).getExperience()));
+        holder.txtName.setText(lutemons.get(position).getName() + " (" + lutemons.get(position).getColor() + ")");
+        holder.txtAttack.setText("Hyökkäys: " + lutemons.get(position).getAttack());
+        holder.txtDefence.setText("Puolustus: " + lutemons.get(position).getDefense());
+        holder.txtHealth.setText("Elämä: " + lutemons.get(position).getHealth() + "/" + lutemons.get(position).getMaxHealth());
+        holder.txtExperience.setText("Kokemus: " + lutemons.get(position).getExperience());
         //holder.lutemon1.setImageResource(lutemons.get(position).getImage()); TODO laita tämä toimimaan
 
     }
