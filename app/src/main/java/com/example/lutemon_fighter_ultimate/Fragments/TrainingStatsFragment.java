36e2c06c8d0bf02867eaf2e463e147dc3f85adc8
eemil-rii <eemil.riihimaki@gmail.com>
2023-04-22 11:33:05 +0300

    Treenaaminen

---
+++ app/src/main/java/com/example/lutemon_fighter_ultimate/Fragments/TrainingStatsFragment.java
@@ -0,0 +1,33 @@
+package com.example.lutemon_fighter_ultimate.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lutemon_fighter_ultimate.R;
+
+
+public class TrainingStatsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_training_stats, container, false);
+
+        return view;
+    }
+}
